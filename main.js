2be277ab6efce8c46a129b9b319b81b571296cc7
amazingcoderNadir <95256318+amazingcoderNadir@users.noreply.github.com>
2022-08-31 15:57:37 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,25 +1,18 @@
 function preload() {
-}
+} 
 
 function setup() {
-    canvas = createCanvas(640, 480);
+    canvas =  createCanvas(640, 480);
     canvas.position(110, 250);
     video = createCapture(VIDEO);
     video.hide();
-
-    tint_color = "";
 }
-
 function draw() {
     image(video, 0, 0, 640,480);
-    tint(tint_color);
-}
-
-function take_snapshot() {
-    save('student_name.png');
-}
-
-function filter_tint()
-{
-    tint_color = document.getElementById("color_name").value;
+    fill("red");
+    stroke("red");
+    circle(20, 20, 75 );
+    circle(600, 20, 75 );
+    circle(20, 450, 75 );
+    circle(600, 450, 75 );
 }
